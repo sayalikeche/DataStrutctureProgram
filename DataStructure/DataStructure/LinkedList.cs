b9a11efb7901910f0c9a1f2d1c8e5e7f93621329
sayalikeche <kechesayali11@gmail.com>
2021-11-21 19:50:02 +0530

    [sayali added Delet First Element]

--- DataStructure/DataStructure/LinkedList.cs
+++ DataStructure/DataStructure/LinkedList.cs
@@ -80,5 +80,14 @@ namespace Datastructure
             prev.next = newestNode;
             return this.head;
         }
+        internal Node RemoveFirstNode()
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            return this.head;
+        }
     }
 }
