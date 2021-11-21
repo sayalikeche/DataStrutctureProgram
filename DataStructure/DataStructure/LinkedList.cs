d6ddea4314baf1e6b671b7d6ee3056f35d4910fa
sayalikeche <kechesayali11@gmail.com>
2021-11-21 17:35:20 +0530

    [sayali added Data Structure Program]

---
+++ DataStructure/DataStructure/LinkedList.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Datastructure
+{
+    internal class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.Write("\n {0} inserted into linked list", node.data);
+        }
+        public void AddinreverseOrder(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newNode;
+                head.next = temp;
+            }
+            Console.Write("\n {0} inserted into linked list", newNode.data);
+        }
+        internal void Display()
+        {
+            Console.Write("\n Data of linked list : ");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            Node newestNode = new Node(data);
+            if (this.head == null)
+            {
+                return newestNode;
+            }
+            if (position == 0)
+            {
+                newestNode.next = this.head;
+                this.head = newestNode;
+                return this.head;
+            }
+            Node prev = null;
+            Node current = this.head;
+            int count = 0;
+            while (current != null && count < position)
+            {
+                prev = current;
+                current = current.next;
+                count++;
+            }
+            newestNode.next = prev.next;
+            prev.next = newestNode;
+            return this.head;
+        }
+        internal Node RemoveFirstNode()
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            return this.head;
+        }
+    }
+}
